efcdc6fec8fda39819b7dbe411a83519438e2224
SergioRibera <sergioalejandroriberacosta@gmail.com>
2020-11-09 13:50:57 -0400

    Preview Projects

--- res-home/js/main.js
+++ res-home/js/main.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
-	var contentSections = $('.cd-section'),
-		navigationItems = $('#cd-vertical-nav a');
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
-	});
-	navigationItems.on('click', function(event){
+    var contentSections = $('.cd-section'),
+        navigationItems = $('#cd-vertical-nav a');
+    updateNavigation();
+    $(window).on('scroll', function(){
+        updateNavigation();
+    });
+    navigationItems.on('click', function(event){
         event.preventDefault();
         smoothScroll($(this.hash));
     });
@@ -14,47 +14,49 @@ jQuery(document).ready(function($){
         smoothScroll($(this.hash));
     });
     $('.touch .cd-nav-trigger').on('click', function(){
-    	$('.touch #cd-vertical-nav').toggleClass('open');
-  
+        $('.touch #cd-vertical-nav').toggleClass('open');
+
     });
     $('.touch #cd-vertical-nav a').on('click', function(){
-    	$('.touch #cd-vertical-nav').removeClass('open');
+        $('.touch #cd-vertical-nav').removeClass('open');
     });
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('is-selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('is-selected');
-			}
-		});
-	}
-	function smoothScroll(target) {
+    function updateNavigation() {
+        contentSections.each(function(){
+            $this = $(this);
+            var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+            if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+                navigationItems.eq(activeSection).addClass('is-selected');
+            }else {
+                navigationItems.eq(activeSection).removeClass('is-selected');
+            }
+        });
+    }
+    function smoothScroll(target) {
         $('body,html').animate(
-        	{'scrollTop':target.offset().top},
-        	600
+            {'scrollTop':target.offset().top},
+            600
         );
-	}
-	particlesJS.load('particles-js', 'res-home/js/particlesjs-config.json', ()=>{});
-	var carousel = document.querySelector('.carousel');
-	var flkty = new Flickity( carousel, {
-	imagesLoaded: true,
-	percentPosition: false,
-	});
+    }
+    particlesJS.load('particles-js', 'res-home/js/particlesjs-config.json', ()=>{});
 
-	var imgs = carousel.querySelectorAll('.carousel-cell img');
-	// get transform property
-	var docStyle = document.documentElement.style;
-	var transformProp = typeof docStyle.transform == 'string' ?
-	'transform' : 'WebkitTransform';
-
-	flkty.on( 'scroll', function() {
-	flkty.slides.forEach( function( slide, i ) {
-		var img = imgs[i];
-		var x = ( slide.target + flkty.x ) * -1/3;
-		img.style[ transformProp ] = 'translateX(' + x  + 'px)';
-	});
-	});
+    fetch('https://api.github.com/users/SergioRibera/repos', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }).then(r => r.json()).then(data => {
+        console.log(data);
+    });
 });
+
+const processRepos = (data) => {
+    let container = document.getElementById('pro-content');
+    data.forEach(repo => {
+        let name = repo.name;
+        let urlRepo = repo.html_url;
+        let descrip = repo.description;
+        let isFork = repo.fork;
+
+    });
+}
