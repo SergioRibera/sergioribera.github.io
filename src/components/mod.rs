32c092bd8d89a3021765c1f4d81db528f740ca9c
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-09-03 02:37:28 -0400

    refactor: re-export navigator component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,6 +2,6 @@ mod button;
 mod card;
 mod navigator;
 
-pub use navigator::*;
 pub use button::{Button, ButtonSize, ButtonVariant};
 pub use card::{Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle};
+pub use navigator::Navigator;
